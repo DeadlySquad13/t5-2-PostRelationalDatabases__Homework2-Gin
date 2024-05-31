6a10e62f17bd3ca33473954617268a59e49cefcc
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-05-31 21:47:32 +0300

    [Style](Scripts): Uncommented queries.

--- scripts/gin/experiments.sql
+++ scripts/gin/experiments.sql
@@ -4,7 +4,7 @@
 
 -- 1.1.	С применением индексов, фиксированным количеством записей (1000 шт.) и вариацией селективности (по таблице 1).
 
-/* \o /experiment_data/1.1.json
+\o /experiment_data/1.1.json
 
 -- 1.1.1 Селективность 1%.
 explain(analyze 1, format json, timing 1)
@@ -22,10 +22,10 @@ SELECT event_message from gin.1_users_gin_1k where event_message_tsv @@ to_tsque
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.1_users_gin_1k where event_message_tsv @@ to_tsquery('User1');
 
-\o */
+\o
 
 -- 1.2.	Без применения индексов, фиксированным количеством записей (1000 шт.) и вариацией селективности (по таблице 2).
-/* \o /experiment_data/1.2.json
+\o /experiment_data/1.2.json
 
 -- 1.2.1 Селективность 1%.
 explain(analyze 1, format json, timing 1)
@@ -43,13 +43,13 @@ SELECT event_message from gin.1_users_1k where event_message_tsv @@ to_tsquery('
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.1_users_1k where event_message_tsv @@ to_tsquery('User1');
 
-\o */
+\o
 
 -- 2 В tsquery используется включение слова1 и слова2
 
 -- 2.1.	С применением индексов, фиксированным количеством записей (1000 шт.) и вариацией селективности (по таблице 1).
 
-/* \o /experiment_data/2.1.json
+\o /experiment_data/2.1.json
 
 -- 2.1.1 Селективность 1%.
 explain(analyze 1, format json, timing 1)
@@ -67,10 +67,11 @@ SELECT event_message from gin.1_users_gin_1k where event_message_tsv @@ to_tsque
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.1_users_gin_1k where event_message_tsv @@ to_tsquery('User1 & User2');
 
-\o */
+\o
 
 -- 2.2.	Без применения индексов, фиксированным количеством записей (1000 шт.) и вариацией селективности (по таблице 2).
-/* \o /experiment_data/2.2.json
+
+\o /experiment_data/2.2.json
 
 -- 2.2.1 Селективность 1%.
 explain(analyze 1, format json, timing 1)
@@ -88,11 +89,11 @@ SELECT event_message from gin.1_users_1k where event_message_tsv @@ to_tsquery('
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.1_users_1k where event_message_tsv @@ to_tsquery('User1 & User2');
 
-\o */
+\o
 
 -- 2.3.	С применением индексов, зафиксированной селективностью (10%) и вариацией количества записей (по таблице 1).
 
-/* \o /experiment_data/2.3.json
+\o /experiment_data/2.3.json
 
 -- 2.3.1 Количество записей: 1.000.
 explain(analyze 1, format json, timing 1)
@@ -101,11 +102,12 @@ SELECT event_message from gin.1_users_gin_1k where event_message_tsv @@ to_tsque
 -- 2.3.2 Количество записей: 1.000.000.
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.1_users_gin_1m where event_message_tsv @@ to_tsquery('User1 & User2');
-\o */
+
+\o
 
 -- 2.4.	Без применения индексов, зафиксированной селективностью (10%) и вариацией количества записей (по таблице 2).
 
-/* \o /experiment_data/2.4.json
+\o /experiment_data/2.4.json
 
 -- 2.4.1 Количество записей: 1.000.
 explain(analyze 1, format json, timing 1)
@@ -114,14 +116,14 @@ SELECT event_message from gin.1_users_1k where event_message_tsv @@ to_tsquery('
 -- 2.4.2 Количество записей: 1.000.000.
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.1_users_1m where event_message_tsv @@ to_tsquery('User1 & User2');
-\o */
 
+\o
 
 -- 3 В tsquery используется включение слова1 или слова2
 
 -- 3.1.	С применением индексов, фиксированным количеством записей (1000 шт.) и вариацией селективности (по таблице 1).
 
-/* \o /experiment_data/3.1.json
+\o /experiment_data/3.1.json
 
 -- 3.1.1 Селективность 1%.
 explain(analyze 1, format json, timing 1)
@@ -139,10 +141,11 @@ SELECT event_message from gin.users_gin_1k where event_message_tsv @@ to_tsquery
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.users_gin_1k where event_message_tsv @@ to_tsquery('User1 | User2 | User3 | User4 | User5 | User6 | User7 | User8 | User9 | User10 | User11 | User12 | User13 | User14 | User15 | User16 | User17 | User18 | User19 | User20');
 
-\o */
+\o
 
 -- 3.2.	Без применения индексов, фиксированным количеством записей (1000 шт.) и вариацией селективности (по таблице 2).
-/* \o /experiment_data/3.2.json
+
+\o /experiment_data/3.2.json
 
 -- 3.2.1 Селективность 1%.
 explain(analyze 1, format json, timing 1)
@@ -160,11 +163,11 @@ SELECT event_message from gin.users_1k where event_message_tsv @@ to_tsquery('Us
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.users_1k where event_message_tsv @@ to_tsquery('User1 | User2 | User3 | User4 | User5 | User6 | User7 | User8 | User9 | User10 | User11 | User12 | User13 | User14 | User15 | User16 | User17 | User18 | User19 | User20');
 
-\o */
+\o
 
 -- 3.3.	С применением индексов, зафиксированной селективностью (10%) и вариацией количества записей (по таблице 1).
 
-/* \o /experiment_data/3.3.json
+\o /experiment_data/3.3.json
 
 -- 3.3.1 Количество записей: 1.000.
 explain(analyze 1, format json, timing 1)
@@ -173,11 +176,12 @@ SELECT event_message from gin.users_gin_1k where event_message_tsv @@ to_tsquery
 -- 3.3.2 Количество записей: 1.000.000.
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.users_gin_1m where event_message_tsv @@ to_tsquery('User1 | User2 | User3 | User4 | User5 | User6 | User7 | User8 | User9 | User10');
-\o */
+
+\o
 
 -- 3.4.	Без применения индексов, зафиксированной селективностью (10%) и вариацией количества записей (по таблице 2).
 
-/* \o /experiment_data/3.4.json
+\o /experiment_data/3.4.json
 
 -- 3.4.1 Количество записей: 1.000.
 explain(analyze 1, format json, timing 1)
@@ -187,13 +191,13 @@ SELECT event_message from gin.users_1k where event_message_tsv @@ to_tsquery('Us
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.users_1m where event_message_tsv @@ to_tsquery('User1 | User2 | User3 | User4 | User5 | User6 | User7 | User8 | User9 | User10');
 
-\o */
+\o
 
 -- 4 В tsquery используется слово1 <-> слово2
 
 -- 4.1.	С применением индексов, фиксированным количеством записей (1000 шт.) и вариацией селективности (по таблице 1).
 
-/* \o /experiment_data/4.1.json
+\o /experiment_data/4.1.json
 
 -- 4.1.1 Селективность 1%.
 explain(analyze 1, format json, timing 1)
@@ -211,10 +215,11 @@ SELECT event_message from gin.users_gin_1k where event_message_tsv @@ to_tsquery
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.users_gin_1k where event_message_tsv @@ to_tsquery('User1 <-> tried');
 
-\o */
+\o
 
 -- 4.2.	Без применения индексов, фиксированным количеством записей (1000 шт.) и вариацией селективности (по таблице 2).
-/* \o /experiment_data/4.2.json
+
+\o /experiment_data/4.2.json
 
 -- 4.2.1 Селективность 1%.
 explain(analyze 1, format json, timing 1)
@@ -232,11 +237,11 @@ SELECT event_message from gin.users_1k where event_message_tsv @@ to_tsquery('Us
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.users_1k where event_message_tsv @@ to_tsquery('User1 <-> tried');
 
-\o */
+\o
 
 -- 4.3.	С применением индексов, зафиксированной селективностью (10%) и вариацией количества записей (по таблице 1).
 
-/* \o /experiment_data/4.3.json
+\o /experiment_data/4.3.json
 
 -- 4.3.1 Количество записей: 1.000.
 explain(analyze 1, format json, timing 1)
@@ -245,11 +250,12 @@ SELECT event_message from gin.users_gin_1k where event_message_tsv @@ to_tsquery
 -- 4.3.2 Количество записей: 1.000.000.
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.users_gin_1m where event_message_tsv @@ to_tsquery('User1 <-> tried');
-\o */
+
+\o
 
 -- 4.4.	Без применения индексов, зафиксированной селективностью (10%) и вариацией количества записей (по таблице 2).
 
-/* \o /experiment_data/4.4.json
+\o /experiment_data/4.4.json
 
 -- 4.4.1 Количество записей: 1.000.
 explain(analyze 1, format json, timing 1)
@@ -258,4 +264,5 @@ SELECT event_message from gin.users_1k where event_message_tsv @@ to_tsquery('Us
 -- 4.4.2 Количество записей: 1.000.000.
 explain(analyze 1, format json, timing 1)
 SELECT event_message from gin.users_1m where event_message_tsv @@ to_tsquery('User1 <-> tried');
-\o */
+
+\o
