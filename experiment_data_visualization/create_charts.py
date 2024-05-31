e5cf6de9bc78e630c18b8a4afd9cb73a7b7a9f67
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-05-31 21:47:15 +0300

    [Enh](ExperimentDataVisualization): Added different types of charts.

--- experiment_data_visualization/create_charts.py
+++ experiment_data_visualization/create_charts.py
@@ -22,17 +22,59 @@ def read_experiments_data(json_file: Path):
     ]
 
 
-def main():
-    experiment_data_processed_path = Path('../experiment_data_processed')
-    experiment_data = read_experiments_data(experiment_data_processed_path / '3.2.json')
-    print(experiment_data)
+SELECTION_PERCENTS = ["1", "3", "10", "20"]
+experiment_data_processed_path = Path("../experiment_data_processed")
+
+
+def selection_charts():
+    experiment_data_gin = read_experiments_data(
+        experiment_data_processed_path / "3.1.json"
+    )
+    total_times_gin = [data["total"] for data in experiment_data_gin]
+    execution_times_gin = [data["execution"] for data in experiment_data_gin]
+
+    experiment_data = read_experiments_data(experiment_data_processed_path / "3.2.json")
     total_times = [data["total"] for data in experiment_data]
+    execution_times = [data["execution"] for data in experiment_data]
 
     fig, ax = plt.subplots()  # Create a figure containing a single Axes.
-    ax.plot(['1%', '3%', '10%', '20%'], total_times, label="Actual Total Time")
-    # ax.plot([1, 2, 3, 4], [4, 4, 4, 3], label="2")
+    ax.plot(SELECTION_PERCENTS, total_times_gin, label="Actual Total Time (GIN)")
+    ax.plot(SELECTION_PERCENTS, execution_times_gin, label="Execution (GIN)")
+    ax.plot(SELECTION_PERCENTS, total_times, label="Actual Total Time")
+    ax.plot(SELECTION_PERCENTS, execution_times, label="Execution")
     ax.legend()
+    ax.set_xlabel('Селективность, %')
+    ax.set_ylabel('Время, мс')
     plt.show()  # Show the figure.
+
+
+NUMBER_OF_ROWS = ['1.000', '1.000.000']
+
+
+def number_of_rows_charts():
+    experiment_data_gin = read_experiments_data(
+        experiment_data_processed_path / "3.3.json"
+    )
+    total_times_gin = [data["total"] for data in experiment_data_gin]
+    execution_times_gin = [data["execution"] for data in experiment_data_gin]
+
+    experiment_data = read_experiments_data(experiment_data_processed_path / "3.4.json")
+    total_times = [data["total"] for data in experiment_data]
+    execution_times = [data["execution"] for data in experiment_data]
+
+    fig, ax = plt.subplots()  # Create a figure containing a single Axes.
+    ax.plot(NUMBER_OF_ROWS, total_times_gin, label="Actual Total Time (GIN)")
+    ax.plot(NUMBER_OF_ROWS, execution_times_gin, label="Execution (GIN)")
+    ax.plot(NUMBER_OF_ROWS, total_times, label="Actual Total Time")
+    ax.plot(NUMBER_OF_ROWS, execution_times, label="Execution")
+    ax.legend()
+    ax.set_xlabel('Количество записей в таблице')
+    ax.set_ylabel('Время, мс')
+    plt.show()  # Show the figure.
+
+
+def main():
+    number_of_rows_charts()
 
 
 if __name__ == "__main__":
